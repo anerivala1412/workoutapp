345de0a6ca5a85987bcf7d13ee5789bca8d16736
aneri <aneriavala26@gmail.com>
2023-03-28 11:53:38 +0530

    AWS image upload done with jsV3 version

--- app/middlewares/imageUpload.js
+++ app/middlewares/imageUpload.js
@@ -1,15 +1,14 @@
-const aws = require("aws-sdk");
+require('dotenv').config();
 const multer = require("multer");
 const multerS3 = require("multer-s3");
-
-const s3 = new aws.S3();
-
-aws.config.update({
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_ACCESS_SECRET,
+const { S3Client } = require('@aws-sdk/client-s3')
+const s3Client = new S3Client({
     region: process.env.S3_ORIGIN,
-});
-
+    credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_ACCESS_SECRET,
+    }
+})
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
         cb(null, true);
@@ -21,9 +20,9 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     fileFilter,
     storage: multerS3({
-        acl: "public-read",
-        s3,
-        bucket: process.env.S3_ORIGIN,
+        acl: process.env.S3_PERMISSION,
+        s3: s3Client,
+        bucket: process.env.S3_BUCKET,
         metadata: function(req, file, cb) {
             console.log({ output: req })
             cb(null, { fieldName: "TESTING_METADATA" });
