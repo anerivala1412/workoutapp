b3ef1e84d34d18c1f2d2d657ecdf9936f9bb101f
jone557 <ethiofanjo@gmail.com>
2023-04-25 18:01:49 +0530

    updated the image/query meal search

--- app/controllers/chatgpt.image.controller.js
+++ app/controllers/chatgpt.image.controller.js
@@ -12,60 +12,96 @@ const app = new Clarifai.App({
 const apiKey = process.env.NEUTRITION_API_KEY;
 
 //how sure is the api it detected the meal 
-const threshold = 0.75;
+const threshold = 0.9;
 const InputImg = 'https://www.spiceupthecurry.com/wp-content/uploads/2020/07/shahi-paneer-recipe-2.jpg'
 
 
 
+function nutritionImageSearch(foodName) {
+    const encodedFoodName = encodeURIComponent(foodName);
+  const url = `${process.env.NEUTRITION_BASE_URL}?query=${encodedFoodName}&pageSize=1&api_key=${apiKey}`;
 
-function nutritionSearch(nutritionInfoPromises) {
-    Promise.all(nutritionInfoPromises)
-        .then(responses => {
-            const nutritionInfo = responses.map(response => ({
-                name: response.data.foods[0].description,
-                calories: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Energy').value,
-                fat: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Total lipid (fat)').value,
-                protein: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Protein').value,
-                carbs: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Carbohydrate, by difference').value
-            }));
-            console.log(nutritionInfo);
-            const totalCalories = nutritionInfo.reduce((sum, info) => sum + info.calories, 0);
-            const totalFat = nutritionInfo.reduce((sum, info) => sum + info.fat, 0);
-            const totalProtein = nutritionInfo.reduce((sum, info) => sum + info.protein, 0);
-            const totalCarbs = nutritionInfo.reduce((sum, info) => sum + info.carbs, 0);
-
-
-            console.log(`Total calories: ${totalCalories}`);
-            console.log(`Total fat: ${totalFat}`);
-            console.log(`Total protein: ${totalProtein}`);
-            console.log(`Total carbs: ${totalCarbs}`);
-            return `Total calories: ${totalCalories},Total fat: ${totalFat},Total protein: ${totalProtein}, Total carbs: ${totalCarbs}`
-        })
-        .catch(error => {
-            return res.status(500).send({
-                message: error.message
-            }) 
-        });
+  return axios.get(url)
+    .then(response => {
+      const nutritionInfo = {
+        name: response.data.foods[0].description,
+        servingSize: response.data.foods[0].servingSize,
+        calories: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Energy')?.value,
+        fat: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Total lipid (fat)')?.value,
+        protein: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Protein')?.value,
+        carbs: response.data.foods[0].foodNutrients.find(nutrient => nutrient.nutrientName === 'Carbohydrate, by difference')?.value
+      };
+      console.log(nutritionInfo);
+      return nutritionInfo;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
 }
 
-exports.getImageResult = (req, res) => {
-    app.models.predict(Clarifai.FOOD_MODEL, req.body.imageUrl)
+exports.getImageResult = async (req, res) => {
+    if(req.body.imageUrl){
+        app.models.predict(Clarifai.FOOD_MODEL, req.body.imageUrl)
         .then(response => {
             const foodNames = response.outputs[0].data.concepts
                 .filter(concept => concept.name !== undefined && concept.value >= threshold)
                 .map(concept => concept.name);
             console.log(foodNames);
             const nutritionInfoPromises = foodNames.map(foodName => {
-                const encodedFoodName = encodeURIComponent(foodName);
-                const url = `${process.env.NEUTRITION_BASE_URL}?query=${encodedFoodName}&pageSize=1&api_key=${apiKey}`;
-                return axios.get(url);
-            });
-            const items = nutritionSearch(nutritionInfoPromises);
-            return res.status(200).send({
-                items
+                return nutritionImageSearch(foodName);
+              });
+            return Promise.all(nutritionInfoPromises)
+            .then(nutritionInfoList => {
+                const totalNutrition = {
+                    totalCalories: 0,
+                    totalFat: 0,
+                    totalProtein: 0,
+                    totalCarbs: 0
+                  };
+                  for (let i = 0; i < nutritionInfoList.length; i++) {
+                    const nutritionInfo = nutritionInfoList[i];
+                    const servingSize = nutritionInfo.servingSize || 10; // default serving size is 100g
+                    totalNutrition.totalCalories += (nutritionInfo.calories || 0) * (servingSize / 100);
+                    totalNutrition.totalFat += (nutritionInfo.fat || 0) * (servingSize / 100);
+                    totalNutrition.totalProtein += (nutritionInfo.protein || 0) * (servingSize / 100);
+                    totalNutrition.totalCarbs += (nutritionInfo.carbs || 0) * (servingSize / 100);
+                  }
+                  console.log(`Total calories: ${totalNutrition.totalCalories}`);
+                  console.log(`Total fat: ${totalNutrition.totalFat}`);
+                  console.log(`Total protein: ${totalNutrition.totalProtein}`);
+                  console.log(`Total carbs: ${totalNutrition.totalCarbs}`);
+      
+              return { items: totalNutrition, ingredients: foodNames };
+            })
+            .catch(error => {
+              throw new Error(error.message);
             });
         })
+        .then(data => {
+          return res.status(200).send(data);
+        })
         .catch(err => {
-            console.log(err);
+          return res.status(500).send({
+            message: err.message
+          });
         });
+    }else{
+        if (!req.query.foodName) {
+            return res.status(500).send({ errors: 'No Image or Food Name provided' });
+        }
+        const encodedFoodName = encodeURIComponent(req.query.foodName);
+         const url = `${process.env.NEUTRITION_BASE_URL}?query=${encodedFoodName}&pageSize=5&api_key=${apiKey}`;
+        const neutritionInfoPromise = await axios.get(url);
+        const allfoods = neutritionInfoPromise.data.foods
+        const nutritionInfo = allfoods.map(food=>({
+            name: food.description,
+            ingredients: food.ingredients,
+            calories: food.foodNutrients.find(nutrient => nutrient.nutrientName === 'Energy').value,
+            fat: food.foodNutrients.find(nutrient => nutrient.nutrientName === 'Total lipid (fat)').value,
+            protein: food.foodNutrients.find(nutrient => nutrient.nutrientName === 'Protein').value,
+            carbs: food.foodNutrients.find(nutrient => nutrient.nutrientName === 'Carbohydrate, by difference').value
+        }))
+        return res.send( {queryItems: nutritionInfo })
+    }
+    
 }
--- app/routes/meal.image.routes.js
+++ app/routes/meal.image.routes.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const controller = require("../controllers/chatgpt.image.controller");
+const mediaUpload = require("../middlewares/media");
+const singleImageUpload = mediaUpload.imageUpload.single('image');
 module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header(
@@ -9,8 +11,21 @@ module.exports = function(app) {
         next();
     });
 
-    app.post(
-        "/api/image-result", [],
+    app.get(
+        "/api/image-result", function(req, res, next) {
+            if (req.headers['content-type']?.startsWith('multipart/form-data')) {
+            singleImageUpload(req, res, function(err, some) {
+                if (err) {
+                    return res.status(422).send({ errors: [{ title: 'Image Upload Error', detail: err.message }] });
+                }
+                req.body.imageUrl = req.file.location;
+                next();
+                
+            });
+        }else{
+           next()
+        }
+        },
         controller.getImageResult
     );
 
